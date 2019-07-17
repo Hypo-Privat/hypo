cc066d97fa57831b8ebd6aa79c845f14b2c264a8
Drummer <37729697+Hypo-Privat@users.noreply.github.com>
2019-07-17 18:20:51 +0000

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -10,9 +10,8 @@ $datum = date("Y-m-d", $timestamp);
 $uhrzeit = date("H:i:s", $timestamp);
 echo $datum, " - ", $uhrzeit, " Uhr <br>";
 
-$db = mysqli_connect("server11.hostfactory.ch", "hypo_usr", "Tekki-1234", "hypo");
-// $db = mysqli_connect ("localhost", "phpmyadmin", "Name0815", "phpmyadmin");
-// $db = mysqli_connect ( "www.jamfinder.info", "jamfinder_usr", "Name0815", "jamfinder" );
+$db = mysqli_connect("server11.xxx.ch", "hypo_usr", "Txx", "hypo");
+
 if (mysqli_connect_errno()) {
     printf("Verbindung fehlgeschlagen: %s", mysqli_connect_error());
     exit();
